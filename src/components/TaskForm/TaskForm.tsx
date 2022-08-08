f31bbd9972217cf1dc723ed9f819930aeabc598d
Xifre Font <xifre.font.97@gmail.com>
2022-08-08 22:38:44 +0200

    Added delete function on button

--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -6,7 +6,7 @@ const TaskForm = (): JSX.Element => {
     <TaskFormStyled className="add-task">
       <label htmlFor="add-task__name" className="add-task__label"></label>
       <input type="text" id="add-task__name" className="add-task__name" />
-      <Button type="submit" text="Add" />
+      <Button type="submit" text="Add" action={() => {}} />
     </TaskFormStyled>
   );
 };
--- src/components/TaskItem/TaskItem.tsx
+++ src/components/TaskItem/TaskItem.tsx
@@ -1,4 +1,6 @@
+import { useDispatch } from "react-redux";
 import Task from "../../features/models/Task";
+import { deleteTaskActionNew } from "../../features/reducer/actionCreator";
 import Button from "../Button/Button";
 import TaskItemStyled from "./TaskItemStyled";
 
@@ -7,11 +9,17 @@ interface TaskProps {
 }
 
 const TaskItem = ({ task }: TaskProps): JSX.Element => {
+  const dispatch = useDispatch();
+
+  const deleteTask = () => {
+    dispatch(deleteTaskActionNew(task.id));
+  };
+
   return (
     <TaskItemStyled>
       <article className="task-container">
         <span className="task-name">{task.name}</span>
-        <Button type="button" text="Delete" />
+        <Button type="button" text="Delete" action={deleteTask} />
       </article>
     </TaskItemStyled>
   );
