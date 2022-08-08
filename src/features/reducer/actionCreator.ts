ee6116f4bb493c4eaea44f27a53c2be687853c34
Xifre Font <xifre.font.97@gmail.com>
2022-08-08 20:38:32 +0200

    Created an array of tasks and prepared a load action for the reducer

--- src/features/reducer/actionCreator.ts
+++ src/features/reducer/actionCreator.ts
@@ -1,6 +1,6 @@
 import { createAction } from "@reduxjs/toolkit";
 import Task from "../models/Task";
-import { actionTypes } from "./actionTypes";
+import actionTypes from "./actionTypes";
 
-export const loadTaskActionNew = createAction<Task>(actionTypes.load);
+export const loadTaskActionNew = createAction<Task[]>(actionTypes.load);
 export const addTaskActionNew = createAction<Task>(actionTypes.add);
---
+++ src/utils/tasks.ts
@@ -0,0 +1,26 @@
+import Task from "../features/models/Task";
+
+const tasks: Task[] = [
+  {
+    id: 1,
+    name: "Clean the house",
+    done: false,
+  },
+  {
+    id: 2,
+    name: "Learn redux",
+    done: false,
+  },
+  {
+    id: 3,
+    name: "Kill the noisy neighbours",
+    done: true,
+  },
+  {
+    id: 4,
+    name: "Wash the dishes",
+    done: false,
+  },
+];
+
+export default tasks;
