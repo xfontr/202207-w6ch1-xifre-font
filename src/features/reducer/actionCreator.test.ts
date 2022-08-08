e5c8a8924386eec38f8b8afa7ef96dde8fbc5522
Xifre Font <xifre.font.97@gmail.com>
2022-08-08 20:49:51 +0200

    Tested addTaskActionNew

--- src/features/reducer/actionCreator.test.ts
+++ src/features/reducer/actionCreator.test.ts
@@ -1,8 +1,8 @@
-import { loadTaskActionNew } from "./actionCreator";
+import { addTaskActionNew, loadTaskActionNew } from "./actionCreator";
 
 describe("Given a loadTaskActionNew function", () => {
   describe("When instantiated", () => {
-    test("Then it should return an object with type 'loadtasks' and a payload", () => {
+    test("Then it should return an object with type 'loadtasks' and an array of tasks as a payload", () => {
       const tasks = [
         {
           id: 1,
@@ -17,6 +17,27 @@ describe("Given a loadTaskActionNew function", () => {
       };
 
       const result = loadTaskActionNew(tasks);
+
+      expect(result).toEqual(expectedResult);
+    });
+  });
+});
+
+describe("Given a addTaskActionNew function", () => {
+  describe("When instantiated", () => {
+    test("Then it should return an object with type 'addTasks' and a single task as a payload", () => {
+      const task = {
+        id: 1,
+        name: "",
+        done: false,
+      };
+
+      const expectedResult = {
+        type: "tasks@add",
+        payload: task,
+      };
+
+      const result = addTaskActionNew(task);
 
       expect(result).toEqual(expectedResult);
     });
