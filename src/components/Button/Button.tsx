65bf520ad4b44db912cbb478ed21657840a34f4d
Xifre Font <xifre.font.97@gmail.com>
2022-08-09 11:32:48 +0200

    Changed sonar config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,14 +4,21 @@ interface ButtonProps {
   type: "submit" | "button";
   text: string;
   action: () => void;
+  eventAction?: (event: React.ChangeEvent<any>) => void;
 }
 
-const Button = ({ type, text, action }: ButtonProps): JSX.Element => {
+const Button = ({
+  type,
+  text,
+  action,
+  eventAction = () => {},
+}: ButtonProps): JSX.Element => {
   const buttonAction = (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ): void => {
     event.preventDefault();
     action();
+    eventAction(event);
   };
 
   return (
--- src/components/TaskForm/TaskFormStyled.ts
+++ src/components/TaskForm/TaskFormStyled.ts
@@ -1,20 +1,28 @@
 import styled from "styled-components";
 
 const TaskFormStyled = styled.form`
+  display: flex;
+  justify-content: space-between;
+
   width: 100%;
   padding: var(--container-padding--big);
   background-color: var(--secondary-color);
   border-radius: var(--rad-border--big);
   margin-bottom: var(--container-gap);
+  box-shadow: 10px 10px 30px 10px rgba(0, 0, 0, 0.15);
 
-  & input {
+  & .add-task__name {
     background-color: var(--primary-color);
     border-radius: var(--rad-border--small);
     padding: 0.8rem;
-    border: none;
+    border: solid 1px transparent;
     outline: none;
     margin-right: 1rem;
-    width: 70%;
+    width: 100%;
+  }
+
+  & .add-task__name:focus {
+    border: solid 1px var(--typography-color);
   }
 `;
 
