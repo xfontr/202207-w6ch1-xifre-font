553d38e1a3694b4aa6d541b8f94921ce9c14fcd2
Xifre Font <xifre.font.97@gmail.com>
2022-08-09 16:01:41 +0200

    Tested that the task name actually changes

--- src/components/TaskItem/TaskItem.test.tsx
+++ src/components/TaskItem/TaskItem.test.tsx
@@ -1,7 +1,17 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import {
+  fireEvent,
+  render,
+  renderHook,
+  screen,
+  waitFor,
+} from "@testing-library/react";
 import { act } from "react-dom/test-utils";
-import { Provider } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
+import { JsxEmit } from "typescript";
+import { resourceLimits } from "worker_threads";
 import { store } from "../../app/store";
+import { addTaskActionNew } from "../../features/actionCreator/actionCreator";
+import { selectAllTasks } from "../../features/selectors/selectors";
 import TaskList from "../TaskList/TaskList";
 import TaskItem from "./TaskItem";
 
@@ -13,6 +23,14 @@ afterEach(() => {
   jest.clearAllTimers();
   jest.useRealTimers();
 });
+
+interface WrapperProps {
+  children: JSX.Element | JSX.Element[];
+}
+
+const Wrapper = ({ children }: WrapperProps): JSX.Element => (
+  <Provider store={store}>{children}</Provider>
+);
 
 describe("Given a Task component", () => {
   describe("When instantiated with a task as a prop", () => {
@@ -85,6 +103,63 @@ describe("Given a Task component", () => {
       expect(inputField.value).toBe("Gym");
       expect(editTask).not.toBeInTheDocument();
       expect(submitButton).toBeInTheDocument();
+    });
+  });
+
+  describe("When double clicking the task name and submiting an edited name", () => {
+    test("Then it should display again the task with the new name", async () => {
+      const task = {
+        id: 10,
+        name: "Gym",
+        done: true,
+      };
+
+      const {
+        result: { current: dispatch },
+      } = renderHook(() => useDispatch(), {
+        wrapper: Wrapper,
+      });
+
+      act(() => {
+        dispatch(addTaskActionNew(task));
+      });
+
+      const newTaskName = "Footing";
+
+      const {
+        result: {
+          current: { tasks: initialTasks },
+        },
+      } = renderHook(() => useSelector(selectAllTasks), {
+        wrapper: Wrapper,
+      });
+
+      render(<TaskItem task={initialTasks[0]} />, { wrapper: Wrapper });
+
+      const editTask = screen.getByText("Gym");
+      fireEvent.doubleClick(editTask);
+
+      const inputField: HTMLInputElement = screen.getByRole("textbox");
+      fireEvent.change(inputField, { target: { value: newTaskName } });
+
+      expect(inputField.value).toBe(newTaskName);
+
+      const submitButton = screen.getByRole("button", { name: "Ok" });
+      fireEvent.click(submitButton);
+
+      const {
+        result: {
+          current: { tasks },
+        },
+      } = renderHook(() => useSelector(selectAllTasks), { wrapper: Wrapper });
+
+      expect(inputField).not.toBeInTheDocument();
+      expect(submitButton.textContent).toBe("Delete");
+
+      render(<TaskItem task={tasks[0]} />, { wrapper: Wrapper });
+
+      const modifiedTask = screen.getByText(newTaskName);
+      expect(modifiedTask).toBeInTheDocument();
     });
   });
 });
